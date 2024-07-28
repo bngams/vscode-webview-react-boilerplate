26087460387b1447c4dd8a19d57822cee45fb2f7
bngams <n.boris@devidia.net>
2024-07-28 15:10:09 +0000

    add format rules with prettier and ediorconfig

--- src/ext/extension.ts
+++ src/ext/extension.ts
@@ -6,47 +6,60 @@ import { SidebarWebview } from './sidebar-webview';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "vscode-webview-react-boilerplate" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-webview-react-boilerplate" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'vscode-webview-react-boilerplate.helloWorld',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        'Hello World from vscode-webview-react-boilerplate!'
+      );
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-webview-react-boilerplate.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-webview-react-boilerplate!');
-	});
+  context.subscriptions.push(disposable);
 
-	context.subscriptions.push(disposable);
+  // Command to send text to an existing terminal
+  let sendTextToTerminalCommand = vscode.commands.registerCommand(
+    'extension.openTerminal',
+    (msg: string) => {
+      if (vscode.env.uiKind === vscode.UIKind.Desktop) {
+        // Find an existing terminal
+        let terminal = vscode.window.terminals.find(
+          (t) => t.name === 'Extension terminal'
+        );
+        if (!terminal) {
+          terminal = vscode.window.createTerminal(`Extension Terminal`);
+        }
+        // Show the terminal
+        terminal.show();
+      } else {
+        // Web environment or other unsupported environment
+        vscode.window.showWarningMessage(
+          'Terminal creation is not supported in this environment.'
+        );
+      }
+    }
+  );
 
-	// Command to send text to an existing terminal
-	let sendTextToTerminalCommand = vscode.commands.registerCommand('extension.openTerminal', (msg: string ) => {
-		if (vscode.env.uiKind === vscode.UIKind.Desktop) { 	
-			// Find an existing terminal
-			let terminal = vscode.window.terminals.find(t => t.name === 'Extension terminal');
-			if (!terminal) {
-				terminal = vscode.window.createTerminal(`Extension Terminal`);
-			}
-			// Show the terminal
-			terminal.show();
-		} else {
-			// Web environment or other unsupported environment
-  			vscode.window.showWarningMessage('Terminal creation is not supported in this environment.');
-		}
-	});
+  context.subscriptions.push(sendTextToTerminalCommand);
 
-	context.subscriptions.push(sendTextToTerminalCommand);
-
-	// Register view
-	const sidebarWebview = new SidebarWebview(context?.extensionUri, {});
-	let view = vscode.window.registerWebviewViewProvider(
-		'sidebar-webview',
-		sidebarWebview,
-	);
-	context.subscriptions.push(view);
+  // Register view
+  const sidebarWebview = new SidebarWebview(context?.extensionUri, {});
+  let view = vscode.window.registerWebviewViewProvider(
+    'sidebar-webview',
+    sidebarWebview
+  );
+  context.subscriptions.push(view);
 }
 
 // This method is called when your extension is deactivated
--- src/ext/sidebar-webview.ts
+++ src/ext/sidebar-webview.ts
@@ -1,54 +1,60 @@
-import { WebviewViewProvider, WebviewView, Webview, Uri, EventEmitter, window } from "vscode";
+import {
+  WebviewViewProvider,
+  WebviewView,
+  Webview,
+  Uri,
+  EventEmitter,
+  window,
+} from 'vscode';
 import { Utils } from './utils';
 import * as vscode from 'vscode';
 
 export class SidebarWebview implements WebviewViewProvider {
-	constructor(
-		private readonly extensionPath: Uri,
-		private data: any,
-		private _view: any = null
-	) {}
+  constructor(
+    private readonly extensionPath: Uri,
+    private data: any,
+    private _view: any = null
+  ) {}
 
-	private onDidChangeTreeData: EventEmitter<any | undefined | null | void> = new EventEmitter<any | undefined | null | void>();
+  private onDidChangeTreeData: EventEmitter<any | undefined | null | void> =
+    new EventEmitter<any | undefined | null | void>();
 
-	refresh(context: any): void {
-		this.onDidChangeTreeData.fire(null);
-		this._view.webview.html = this._getHtmlForWebview(this._view?.webview);
-	}
+  refresh(context: any): void {
+    this.onDidChangeTreeData.fire(null);
+    this._view.webview.html = this._getHtmlForWebview(this._view?.webview);
+  }
 
-	// Called when a view first becomes visible
-	resolveWebviewView(webviewView: WebviewView): void | Thenable<void> {
-		webviewView.webview.options = {
-			enableScripts: true,
-			localResourceRoots: [this.extensionPath],
-		};
+  // Called when a view first becomes visible
+  resolveWebviewView(webviewView: WebviewView): void | Thenable<void> {
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this.extensionPath],
+    };
 
-		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-		this._view = webviewView;
-		this.activateMessageListener();
-	}
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+    this._view = webviewView;
+    this.activateMessageListener();
+  }
 
-	private activateMessageListener() {
-		this._view.webview.onDidReceiveMessage((message: any) => {
-			switch (message.action){
-				case 'OPEN_TERMINAL':
-					vscode.commands.executeCommand('extension.openTerminal');
-					break;
-				default:
-					break;
-			}			
-			
-		});
-	}
+  private activateMessageListener() {
+    this._view.webview.onDidReceiveMessage((message: any) => {
+      switch (message.action) {
+        case 'OPEN_TERMINAL':
+          vscode.commands.executeCommand('extension.openTerminal');
+          break;
+        default:
+          break;
+      }
+    });
+  }
 
-	private _getHtmlForWebview(webview: Webview) {
+  private _getHtmlForWebview(webview: Webview) {
+    // Use a nonce to only allow specific scripts to be run
+    const nonce = Utils.getNonce();
 
-		// Use a nonce to only allow specific scripts to be run
-		const nonce = Utils.getNonce();
-
-		// Basic HTML structure
-		return `
+    // Basic HTML structure
+    return `
         	<h1>Hello</h1>
     	`;
-	}
+  }
 }
